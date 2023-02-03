9ac03a6240a4245c1a3ca8bfa9cb3768402a583e
Klaudiusz <muminowy123@gmail.com>
2023-02-03 09:38:29 +0100

    Created dynamic pagination for currency results

--- js/app.js
+++ js/app.js
@@ -1,56 +1,76 @@
-export const resultCurrency = (currency) => {
-        for(const res of currency){
+export const resultCurrency = (currency,page =1) => {
+    const startIndex = (page - 1) * 10;
+    const endIndex = startIndex + 10;
+    const selectedCurrencies = currency.slice(startIndex, endIndex);
+    let numberOfPages = Math.ceil(currency.length / 10);
 
-            const tb = document.querySelector('tbody');
-            tb.classList.add('tbody')
+    const paginationDiv = document.querySelector('.pagination__menu');
 
-            const tr = document.createElement('tr')
-            tr.classList.add('tr')
-
-            const tdName = document.createElement('td')
-            tdName.classList.add('td')
-
-            const nameImg = document.createElement('img')
-            nameImg.classList.add('img')
-            nameImg.src =`${res.img}`
-
-            const tdPrice = document.createElement('td')
-            tdPrice.classList.add('td')
-            tdPrice.innerText = `${res.price}`
-
-            const tdMax = document.createElement('td')
-            tdMax.classList.add('td')
-            tdMax.innerText = `${res.max24}`
-
-            const tdMin = document.createElement('td')
-            tdMin.classList.add('td')
-            tdMin.innerText = `${res.min24}`
-
-            const tdChange = document.createElement('td')
-            if (res.change > 0) {
-                tdChange.innerHTML = `
-                 <i class='bx bx-chevron-up' ></i>
-                 ${res.change} %`;
-                 tdChange.classList.add('positive');
-            } else if (res.change < 0) {
-                tdChange.innerHTML = `
-                <i class='bx bx-chevron-down' ></i>
-                ${res.change} %`;
-                tdChange.classList.add('negative');
-            }
-            else {
-                tdChange.innerHTML = `${res.change} %`
-            }
-
-            tr.appendChild(tdName)
-            tdName.appendChild(nameImg)
-            tr.appendChild(tdPrice)
-            tr.appendChild(tdMax)
-            tr.appendChild(tdMin)
-            tr.appendChild(tdChange)
-            tb.appendChild(tr)
+    const createPagination = () => {
+        while (paginationDiv.firstChild) {
+            paginationDiv.removeChild(paginationDiv.firstChild);
         }
+        for (let i = 1; i <= numberOfPages; i++) {
+            // Tworzymy przycisk dla każdej strony
+            const button = document.createElement("button");
+            button.classList.add('pagination__menu-btn')
+            button.innerHTML = i;
+            button.addEventListener("click", () => {
+                resultCurrency(currency, i);
+            });
+            paginationDiv.appendChild(button);
+        }
+    };
+    createPagination();
+
+    const tb = document.querySelector('tbody');
+    tb.innerHTML = '';
+    tb.classList.add('tbody');
+
+    for (const res of selectedCurrencies) {
+        const tr = document.createElement('tr');
+        tr.classList.add('tr');
+
+        const tdName = document.createElement('td');
+        tdName.classList.add('td');
+
+        const nameImg = document.createElement('img');
+        nameImg.classList.add('img');
+        nameImg.src = `${res.img}`;
+
+        const tdPrice = document.createElement('td');
+        tdPrice.classList.add('td');
+        tdPrice.innerText = `${res.price}`;
+
+        const tdMax = document.createElement('td');
+        tdMax.classList.add('td');
+        tdMax.innerText = `${res.max24}`;
+
+        const tdMin = document.createElement('td');
+        tdMin.classList.add('td');
+        tdMin.innerText = `${res.min24}`;
+
+        const tdChange = document.createElement('td');
+        if (res.change > 0) {
+            tdChange.innerHTML = `
+            <i class='bx bx-chevron-up' ></i>
+            ${res.change} %`;
+            tdChange.classList.add('positive');
+        } else if (res.change < 0) {
+            tdChange.innerHTML = `
+            <i class='bx bx-chevron-down' ></i>
+            ${res.change} %`;
+            tdChange.classList.add('negative');
+        } else {
+            tdChange.innerHTML = `${res.change} %`;
+        }
+
+        tr.appendChild(tdName);
+        tdName.appendChild(nameImg);
+        tr.appendChild(tdPrice);
+        tr.appendChild(tdMax);
+        tr.appendChild(tdMin);
+        tr.appendChild(tdChange);
+        tb.appendChild(tr);
     }
-
-
-
+};
--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,12 @@
 import { resultCurrency } from "./app.js";
 
-const API = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+const API = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false';
 
 let currency;
+let page;
 
 (async () => {
-    const res = await fetch(API);
+    const res = await fetch(API + page);
     const data = await res.json();
     currency = data.map(val => {
         return {
@@ -16,5 +17,5 @@ let currency;
             change: val.price_change_24h.toFixed(3),
         }
     });
-    resultCurrency(currency)
+    resultCurrency(currency,page)
 })();
